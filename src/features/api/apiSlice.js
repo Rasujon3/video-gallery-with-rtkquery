e00165c97056370b0b281f40f913d48903c9f351
Rasujon3 <rasujon3@gmail.com>
2022-10-13 22:33:11 +0600

    get single video

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,13 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000",
+    // baseUrl: "http://localhost:9000",
+    baseUrl: "https://sujonlwsserver.herokuapp.com",
   }),
   endpoints: (builder) => ({
     getVideos: builder.query({
       query: () => "/videos",
     }),
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
+    }),
   }),
 });
 
-export const { useGetVideosQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
